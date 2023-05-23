9095d335f38ec75b0a3c8ed65e4eb1f5984d465b
Anteneh Tilaye <antenehtilaye19@gmail.com>
2023-05-23 17:05:04 +0300

    init project

---
+++ src/main/java/com/black/personalCal/PersonalCalApplication.java
@@ -0,0 +1,13 @@
+package com.black.personalCal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/black/personalCal/PersonalCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.personalCal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
