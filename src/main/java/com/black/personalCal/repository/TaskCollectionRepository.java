723ef55e04d4d303f2d344e0425f33876cc640d5
Anteneh Tilaye <antenehtilaye19@gmail.com>
2023-05-24 11:38:09 +0300

    feat: in memory repository

---
+++ src/main/java/com/black/personalCal/repository/TaskCollectionRepository.java
@@ -0,0 +1,51 @@
+package com.black.personalCal.repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.black.personalCal.model.Task;
+import com.black.personalCal.model.TaskStatus;
+import com.black.personalCal.model.TaskType;
+
+import jakarta.annotation.PostConstruct;
+
+@Repository
+public class TaskCollectionRepository {
+
+    private final List<Task> taskList = new ArrayList<>();
+
+    public TaskCollectionRepository() {
+
+    }
+
+    public List<Task> findAll() {
+        return taskList;
+    }
+
+    public Optional<Task> findById(Integer id) {
+        return taskList.stream().filter(t -> t.id().equals(id)).findFirst();
+    }
+
+    public void save(Task task) {
+        taskList.add(task);
+    }
+
+    @PostConstruct
+    private void init() {
+        for (int i = 0; i < 3; i++) {
+            Task t = new Task(
+                    i+1,
+                    "Create Endpoint for Contract Agreement "+(i+1),
+                    "Create endpoints for contract Agreement of jocker project",
+                    TaskStatus.IDEA,
+                    TaskType.FREELANCE_TASK,
+                    LocalDateTime.now(),
+                    null);
+            taskList.add(t);
+        }
+    }
+}
